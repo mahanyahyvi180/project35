34706ddd432718f1054719120e052563d0a1f4f6
mahanyahyvi180 <139365399+mahanyahyvi180@users.noreply.github.com>
2023-07-31 15:51:57 +0330

    Add files via upload

---
+++ ConsoleApp35/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp35
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var time = new SimpleTime(15, 30, 19);
+            Console.WriteLine(time.BuildString());
+            Console.ReadLine();
+        }
+    }
+
+    public class SimpleTime
+    {
+
+        private int hour;
+        private int minute;
+        private int second;
+
+
+        public SimpleTime(int hour, int minute, int second)
+        {
+            this.hour = hour;
+            this.minute = minute;
+            this.second = second;
+        }
+
+        public string BuildString() => $"{"this.ToUniversalString()",24}:{this.ToUniversalString()}" + $"\n{"ToUniversalString()",24}:{ToUniversalString()}";
+
+        public string ToUniversalString() => $"{this.hour:D2}:{this.minute:D2}:{this.second:D2}";
+    }
+}
